3c71b2f76e98b51d4408530e254ed08e8be5a89f
Serdar <serdarsulan@gmail.com>
2025-10-16 18:00:27 +0300

    fetaure/fetch-improvement has changed
    Refactored manually added table data to use forEach for insertion.

--- StudentInfoSystem.Api/Models/Student.cs
+++ StudentInfoSystem.Api/Models/Student.cs
@@ -5,17 +5,18 @@ namespace StudentInfoSystem.Api.Models
 {
 	public class Student
 	{
-		[Required]
+
+        [Required]
+        [Range(100, 999)]
+        public int Number { get; set; }
+
+        [Required]
         [StringLength(40, MinimumLength = 3)]
         public string Name { get; set; }
 
         [Required]
         [StringLength(40, MinimumLength = 2)]
         public string LastName { get; set; }
-
-        [Required]
-        [Range(100,999)]
-        public int Number { get; set; }
 
         [Required]
         [MaxLength(1)]
